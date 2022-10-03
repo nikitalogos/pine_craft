7cfab25dbbb98f3d7038b841c2e53b956d98f0eb
Nikita Logos <safic2@yandex-team.ru>
2022-10-04 01:41:47 +0300

    Refactoring interface 3

---
+++ examples/gen-part/gen-part.sh
@@ -0,0 +1,6 @@
+mkdir out
+cd out
+
+# beams
+../../../pine-craft.py gen-part -n beam_3 -w 1 -t 3
+../../../pine-craft.py gen-part -n beam_5_sparse -w 1 -t 5 -p "x:1 y:5"
--- install.sh
+++ install.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
+echo "~~~~~~~~~~~~~~~Installing python3.9~~~~~~~~~~~~~~~"
 sudo apt-get install python3.9 python3.9-venv
 
+echo "~~~~~~~~~~~~~~~Creating python3 virtual environment~~~~~~~~~~~~~~~"
 rm -r venv
 python3.9 -m venv venv
 venv/bin/pip install --upgrade pip && venv/bin/pip install -r requirements.txt
 
-# enabling autocompletion - https://pypi.org/project/argcomplete/
+echo "~~~~~~~~~~~~~~~Enabling autocompletion - https://pypi.org/project/argcomplete/ ~~~~~~~~~~~~~~~"
 sudo venv/bin/activate-global-python-argcomplete
 
-THIS_FILE_DIR="$(realpath .)"
-sudo ln -s "${THIS_FILE_DIR_DIR}/pine-craft.sh" /usr/bin/pine-craft
+EXE="/usr/bin/pine-craft"
+
+echo "~~~~~~~~~~~~~~~Creating executable '${EXE}'~~~~~~~~~~~~~~~"
+echo "#!/bin/bash
+# PYTHON_ARGCOMPLETE_OK
+$(realpath .)/venv/bin/python $(realpath .)/pine-craft.py" | sudo tee ${EXE}
+sudo chmod a+x ${EXE}
+echo "~~~~~~~~~~~~~~~Done!~~~~~~~~~~~~~~~"
